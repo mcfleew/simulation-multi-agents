f35176ee5b0fdd6f3f9ff398558d6c96805f7226
mcfleew <fleury.timbe@gmail.com>
2019-03-28 10:55:47 +0100

    Initial commit

---
+++ src/agent.js
@@ -0,0 +1,181 @@
+export class Agent {
+    constructor(id, app, isPredator) {
+        var rank = 1;
+        jssim.SimEvent.call(this, rank);
+        this.id = id;
+        this.app = app;
+        this.space = app.space;
+        var initial_x = Math.floor(Math.random() * Math.floor(app.canvasWidth));
+        var initial_y = Math.floor(Math.random() * Math.floor(app.canvasHeight));
+        this.space.updateAgent(this, initial_x, initial_y);
+        this.speed = app.movementSpeedBunny;
+        this.sight = app.sightDistanceBunny;
+        this.separation_space = 30;
+        this.velocity = new jssim.Vector2D(Math.random(), Math.random());
+        this.isPredator = isPredator;
+        this.border = app.canvasBorder;
+        this.boundaryH = app.canvasHeight;
+        this.boundaryW = app.canvasWidth;
+        this.lastMealTime = 0;
+        //this.size = new jssim.Vector2D(8, 8);
+        //this.color = '#00aa00';
+        if(isPredator){
+            this.speed = app.movementSpeedFox;
+            this.sight = app.sightDistanceFox;
+            //this.color = '#ff0000';
+            //this.size = new jssim.Vector2D(10, 10);
+        }
+    }
+
+    initAgent() {
+        this.__proto__ = Object.create(jssim.SimEvent);
+
+        this.__proto__.removeAgent = function(agentId, isPredator) {
+            function checkAgentId(agent) {
+                return agentId === agent.id;
+            }
+
+            if (isPredator) {
+                this.app.foxes.splice(this.app.bunnies.findIndex(checkAgentId), 1);
+            } else {
+                this.app.bunnies.splice(this.app.bunnies.findIndex(checkAgentId), 1);
+            }
+        };
+
+        this.__proto__.update = function(deltaTime) {
+            var boids = this.space.findAllAgents();
+            var pos = this.space.getLocation(this.id);
+            if(this.isPredator) {
+                if (this.app.foxesDead.includes(this.id)) {
+                    return false;
+                }
+
+                var prey = null;
+                //var min_distance = 10000000; //this.sight
+                for (var boidId in boids)
+                {
+                    if (this.app.bunniesKilled.includes(boidId) || this.app.foxesDead.includes(boidId)) {
+                        continue;
+                    }
+                    var boid = boids[boidId];
+                    if(!boid.isPredator) {
+                        var boid_pos = this.space.getLocation(boid.id);
+                        var distance = pos.distance(boid_pos);
+                        if(this.separation_space > distance) {
+                            // Kill bunny
+                            this.lastMealTime = 0;
+                            this.app.bunniesKilled.push(boidId);
+                            this.removeAgent(boidId, false);
+                        } else if(this.sight > distance) {
+                            this.sight = distance;
+                            prey = boid;
+                        }
+                    } else {
+                        var boid_pos = this.space.getLocation(boid.id);
+                        var distance = pos.distance(boid_pos);
+                        if (distance < this.separation_space)
+                        {
+                            // Separation
+                            this.velocity.x += pos.x - boid_pos.x;
+                            this.velocity.y += pos.y - boid_pos.y;
+                        }
+                    }
+                }
+                if(prey != null) {
+                    var prey_position = this.space.getLocation(prey.id);
+                    this.velocity.x += prey_position.x - pos.x;
+                    this.velocity.y += prey_position.y - pos.y;
+                }
+                this.lastMealTime++;
+                if (this.lastMealTime > this.app.survivalTimeFox) {
+                    // Starving to death
+                    this.app.foxesDead.push(this.id);
+                    this.removeAgent(this.id, true);
+                }
+                
+            } else {
+                if (this.app.bunniesKilled.includes(this.id)) {
+                    return false;
+                }
+
+                for (var boidId in boids)
+                {
+                    if (this.app.bunniesKilled.includes(this.id) || this.app.foxesDead.includes(this.id)) {
+                        continue;
+                    }
+                    var boid = boids[boidId];
+                    var boid_pos = this.space.getLocation(boid.id);
+                    var distance = pos.distance(boid_pos);
+                    if (boid != this && !boid.isPredator)
+                    {
+                        if (distance < this.separation_space)
+                        {
+                            // Separation
+                            this.velocity.x += pos.x - boid_pos.x;
+                            this.velocity.y += pos.y - boid_pos.y;
+                        }
+                        else if (distance < this.sight)
+                        {
+                            // Cohesion
+                            this.velocity.x += (boid_pos.x - pos.x) * 0.05;
+                            this.velocity.y += (boid_pos.y - pos.y) * 0.05;
+                        }
+                        if (distance < this.sight)
+                        {
+                            // Alignment
+                            this.velocity.x += boid.velocity.x * 0.5;
+                            this.velocity.y += boid.velocity.y * 0.5;
+                        }
+                    }
+                    if (boid.isPredator && distance < this.sight)
+                    {
+                        // Avoid predators.
+                        this.velocity.x += pos.x - boid_pos.x;
+                        this.velocity.y += pos.y - boid_pos.y;
+                    }
+                }
+            }
+            // check speed
+            var speed = this.velocity.length();
+            if(speed > this.speed) {
+                this.velocity.resize(this.speed);
+            }
+            
+            pos.x += this.velocity.x;
+            pos.y += this.velocity.y;
+              
+            // check boundary
+            var valW = this.boundaryW - this.border;
+            var valH = this.boundaryH - this.border;
+            if (pos.x < this.border) pos.x = valW;
+            if (pos.y < this.border) pos.y = valH;
+            if (pos.x > valW) pos.x = this.border;
+            if (pos.y > valH) pos.y = this.border;
+            //console.log("boid [ " + this.id + "] is at (" + pos.x + ", " + pos.y + ") at time " + this.time);
+        };
+
+        var fox_icon = new Image();
+        fox_icon.src = 'fox.png';
+
+        if (this.app.avatarFileNameFox) {
+            fox_icon.src = this.app.avatarFileNameFox[0].name;
+        }
+
+        var bunny_icon = new Image();
+        bunny_icon.src = 'bunny.png';
+
+        if (this.app.avatarFileNameBunny) {
+            bunny_icon.src = this.app.avatarFileNameBunny[0].name;
+        }
+                
+        this.__proto__.draw = function(context, pos) {
+            if (this.isPredator) {
+                context.drawImage(fox_icon, pos.x, pos.y);
+            } else {
+                context.drawImage(bunny_icon, pos.x, pos.y);
+            }
+            
+        };
+    }
+}
+  
---
+++ src/app.js
@@ -0,0 +1,268 @@
+import {Agent} from './agent';
+
+export class App {
+    constructor() {
+        this.refreshCanvas = null;
+        this.refreshChart = 0;
+        this.chart = null;
+        this.scheduler = new jssim.Scheduler();
+        this.space = new jssim.Space2D();
+
+        this.avatarFileNameBunny = "";
+        this.avatarFileNameFox = "";
+
+        this.simulationTime = 0;
+        this.canvasBorder = 10;
+        this.canvasHeight = 400;
+        this.canvasWidth = 800;
+
+        this.showCanvas = false;
+        this.showForm = false;
+        this.showTitle = true;
+        this.showOptions = false;
+        this.showChart = false;
+
+        this.bunnies = [];
+        this.bunniesKilled = [];
+        this.foxes = [];
+        this.foxesDead = [];
+
+        this.lastMeasuresLabels = [];
+        this.lastMeasuresBunnies = [];
+        this.lastMeasuresFoxes = [];
+
+        this.initialNumberOfBunnies = 0;
+        this.initialNumberOfFoxes = 0;
+        this.respawnFrequencyBunny = 0;
+        //this.respawnFrequencyFox = 0;
+        this.sightDistanceBunny = 0;
+        this.sightDistanceFox = 0;
+        //this.survivalTimeBunny = 0;
+        this.survivalTimeFox = 0;
+        this.movementSpeedBunny = 0;
+        this.movementSpeedFox = 0;
+
+        this.chartUpdateFrequency = 0;
+        this.chartUpdateMeasurement = 0;
+        this.chartLastMeasuresNumber = 0;
+
+        this.updateFrequencies = [
+            { frequency: 1 },
+            { frequency: 10 },
+            { frequency: 20 },
+            { frequency: 30 }
+        ];
+        this.updateMeasurement = [
+            { measurement:1, title: 'Secondes' },
+            { measurement:60, title: 'Minutes' },
+            { measurement:3600, title: 'Heures' }
+        ];
+        this.numberOfMeasures = [
+            { count: 10 },
+            { count: 25 },
+            { count: 50 },
+            { count: 100 }
+        ];
+        this.respawnLevels = [
+            { frequency: 1000, title: 'Très très lente' },
+            { frequency: 500, title: 'Très lente' },
+            { frequency: 250, title: 'Lente' },
+            { frequency: 100, title: 'Normale' },
+            { frequency: 50, title: 'Rapide' },
+            { frequency: 25, title: 'Très rapide' },
+            { frequency: 10, title: 'Très très rapide' },
+            { frequency: 0, title: 'Pas d\'apparition' }
+        ];
+    }
+
+    get respawnFrequencyFox() {
+        return (this.respawnFrequencyBunny * 10);
+    }
+
+    get chartUpdateInterval() {
+        return (100 * this.chartUpdateMeasurement * this.chartUpdateFrequency);
+    }
+
+    initForm() {
+        this.resetForm();
+
+        this.showTitle = false;
+        this.showForm = true;
+    }
+
+    initCanvas(resetCanvas) {
+        if (resetCanvas) {
+            this.resetCanvas();
+            this.resetChart();
+        }
+
+        this.chart = this.drawChart();
+
+        this.showCanvas = true;
+        this.showForm = false;
+
+        this.playCanvas(100);
+    }
+
+    initAgents() {
+        for(var i = 0; i < this.initialNumberOfBunnies; ++i) {
+            this.addBunny('B0_'+i);
+        }
+
+        for(var j = 0; j < this.initialNumberOfFoxes; ++j) {
+            this.addFox('F0_'+j);
+        }
+    }
+
+    addBunny(Id) {
+        var bunny = new Agent(Id, this, false);
+        bunny.initAgent();
+        this.bunnies.push(bunny);
+        this.scheduler.scheduleRepeatingIn(bunny, 1);
+    }
+
+    addFox(Id) {
+        var fox = new Agent(Id, this, true);
+        fox.initAgent();
+        this.foxes.push(fox);
+        this.scheduler.scheduleRepeatingIn(fox, 1);
+    }
+
+    playCanvas(step) {
+        clearInterval(this.refreshCanvas);
+        this.refreshChart = 0;
+
+        var canvas = document.getElementById("myCanvas");
+    
+        var self = this;
+
+        if (step > 0) {
+            self.refreshCanvas = setInterval(function(){
+                if (self.scheduler.current_time > 0) {
+                    if (self.scheduler.current_time % self.respawnFrequencyBunny === 0) {
+                        self.addBunny('B0'+self.scheduler.current_time);
+                    }
+                    if (self.scheduler.current_time % self.respawnFrequencyFox === 0) {
+                        self.addFox('F0'+self.scheduler.current_time);
+                    }
+                    
+                    if (self.refreshChart % self.chartUpdateInterval === 0) {
+                        var tooManyItems = (self.chart.data.labels.length >= self.chartLastMeasuresNumber && self.chartLastMeasuresNumber > 0);
+                    
+                        if (tooManyItems) {
+                            self.chart.data.labels.shift();
+                        }
+
+                        self.chart.data.labels.push(self.scheduler.current_time);
+                        self.chart.data.datasets.forEach((dataset) => {
+                            if (tooManyItems) {
+                                dataset.data.shift();
+                            }
+                            
+                            if (dataset.label.endsWith('lapins')) {
+                                dataset.data.push(self.bunnies.length);
+                            }
+                            if (dataset.label.endsWith('renards')) {
+                                dataset.data.push(self.foxes.length);
+                            }
+                        });
+                        self.chart.update();
+                    }
+                    self.refreshChart += step / 10;
+                }
+    
+                self.scheduler.update();
+                
+                self.space.render(canvas);
+                //console.log('current simulation time: ' + self.scheduler.current_time);
+                self.simulationTime = self.scheduler.current_time;
+            }, step);
+        }
+    }
+
+    stopCanvas() {
+        clearInterval(this.refreshCanvas);
+        this.refreshChart = 0;
+
+        this.showCanvas = false;
+        this.showForm = true;
+    }
+
+    drawChart() {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: this.lastMeasuresLabels,
+                datasets: [{
+                    label: 'Nombre de lapins',
+                    //backgroundColor: 'rgb(255, 199, 132)',
+                    borderColor: 'rgb(255, 199, 132)',
+                    data: this.lastMeasuresBunnies
+                },{
+                    label: 'Nombre de renards',
+                    //backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: this.lastMeasuresFoxes
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+
+        return chart;
+    }
+
+    resetForm() {
+        this.initialNumberOfBunnies = 15;
+        this.initialNumberOfFoxes = 3;
+        this.respawnFrequencyBunny = 100;
+        //this.respawnFrequencyFox = 1000;
+        this.sightDistanceBunny = 75;
+        this.sightDistanceFox = 75;
+        //this.survivalTimeBunny = 10000;
+        this.survivalTimeFox = 200;
+        this.movementSpeedBunny = 12;
+        this.movementSpeedFox = 24;
+
+        this.avatarFileNameBunny = "";
+        this.avatarFileNameFox = "";
+        this.canvasHeight = 400;
+        this.canvasWidth = 800;
+        this.showChart = false;
+
+        this.chartUpdateFrequency = 1;
+        this.chartUpdateMeasurement = 1;
+        this.chartLastMeasuresNumber = 25;
+    }
+
+    resetCanvas() {
+        this.bunnies = [];
+        this.bunniesKilled = [];
+        this.foxes = [];
+        this.foxesDead = [];
+
+        this.scheduler.reset();
+        this.space.reset();
+        this.initAgents();
+    }
+
+    resetChart() {
+        this.lastMeasuresLabels = [];
+        this.lastMeasuresBunnies = [];
+        this.lastMeasuresFoxes = [];
+    }
+
+    showMoreOptions(showOptions) {
+        if (showOptions) {
+            this.showOptions = true;
+        } else {
+            this.showOptions = false;
+        }
+    }
+}
+  
